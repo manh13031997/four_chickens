166edbb87805173e3dd935ceadd6334891e7fd14
08201356 <nguyenhoainma0613@gmail.com>
2023-12-05 21:15:29 +0700

    aa

--- src/main/resources/templates/cart/script.js
+++ src/main/resources/templates/cart/script.js
@@ -1,7 +1,7 @@
 const btn = document.querySelectorAll("button")
 //console.log(btn)
 btn.forEach(function(button,index){
-    console.log(button, index)
+    //console.log(button, index)
     button.addEventListener("click", function (event){
         var btnItem = event.target
         var product = btnItem.parentElement
